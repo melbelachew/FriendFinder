9fb307bc5f9962bc81c2ce243392dbbff4876a50
Melat Belachew <melbelachew@gmail.com>
2019-01-10 23:19:22 -0500

    update

---
+++ routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(tableData);
+    });
+}
+
+for(var i=0; i<friends.length; i++) {
+friends.push(req.body);
+res.json(true)}
